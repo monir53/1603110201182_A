46e89795c4afabf834d26e4e9c8870c6feffaac9
monir53 <kazisaim@gmail.com>
2020-11-21 12:37:25 +0600

    final

---
+++ database/migrations/2020_11_21_051554_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('salary');
+            $table->string('status');
+            $table->foreign('department_id');
+                  ->references('id')->on('departments')
+                  onDelete('cascode');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
